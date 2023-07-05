40accfb6a8dd7e6f3fd7d6e9cf7d153b7b64e742
Ahmad Faisal <maulidae.rizki@gmail.com>
2023-07-05 19:15:17 +0700

    first commit

---
+++ app/View/Components/AppLayout.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use App\Models\Category;
+use Illuminate\View\Component;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Contracts\View\View;
+
+class AppLayout extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(public ?string $metaTitle = null, public ?string $metaDescription = null)
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        $categories = Category::query()
+            ->join('category_posts', 'categories.id', '=', 'category_posts.category_id')
+            ->select('categories.title', 'categories.slug', DB::raw('count(*) as total'))
+            ->groupBy('categories.id', 'categories.title', 'categories.slug')
+            ->orderByDesc('total')
+            ->get();
+
+        return view('layouts.app', compact('categories'));
+    }
+}
